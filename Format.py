23b1208441d4fa511a3fae0292549640f3608488
Tong ZHAN <zhantong1994@163.com>
2017-06-27 17:11:43 +0800

    增加对learning结果的处理

--- Format.py
+++ Format.py
@@ -25,8 +25,34 @@ class Format:
                     result['file']['sha1'] = log_content
         with open(output_file_path, 'w', encoding='utf-8') as f:
             f.write(json.dumps(result))
-
+    def format_log_learning(self,demodulate_log_file_path,learning_log_file_path,output_file_path='output.json'):
+        with open(demodulate_log_file_path,'r',encoding='utf-8') as f:
+            data=json.loads(f.read())
+        log_reg = re.compile(r'(.*?)\s\[(.*?)\]\s(.*?)\s(.*?)\s\[(.*?)\]\s\-\s(.*?)$')
+        learned_frames={}
+        with open(learning_log_file_path,'r',encoding='utf-8') as f:
+            for line in f:
+                res = log_reg.findall(line)
+                log_type = res[0][4]
+                log_content = res[0][5]
+                if log_type == 'barcodeConfig':
+                    data['barcodeConfig'] = json.loads(log_content)
+                elif log_type == 'fecParameters':
+                    data['fecParameters'] = json.loads(log_content)
+                elif log_type == 'processed':
+                    current=json.loads(log_content)
+                    learned_frames[current['index']]=current
+                elif log_type == 'sha1':
+                    data['file']['sha1'] = log_content
+        for frame in data['frames']:
+            index=frame['image']['index']
+            if index in learned_frames:
+                frame['rs']=learned_frames[index]['rs']
+                frame['raptorQ']=learned_frames[index]['raptorQ']
+        with open(output_file_path, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(data))
 
 if __name__ == '__main__':
     format = Format()
-    format.format_log('2017-06-22 20-15-02.txt', 'output.json')
+    #format.format_log('2017-06-22 20-15-02.txt', 'output.json')
+    format.format_log_learning('output.json','2017-06-26 18-05-42.txt','output.json')
