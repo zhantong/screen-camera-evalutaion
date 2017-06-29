19b11a3c42e0b43622cf4e62344355f58e6ad95a
Tong ZHAN <zhantong1994@163.com>
2017-06-29 21:53:10 +0800

    修改对机器学习log的整理，统一格式 只记录较少的结果，与format_log()兼容

--- Format.py
+++ Format.py
@@ -25,34 +25,28 @@ class Format:
                     result['file']['sha1'] = log_content
         with open(output_file_path, 'w', encoding='utf-8') as f:
             f.write(json.dumps(result))
-    def format_log_learning(self,demodulate_log_file_path,learning_log_file_path,output_file_path='output.json'):
-        with open(demodulate_log_file_path,'r',encoding='utf-8') as f:
-            data=json.loads(f.read())
+    def format_log_learning(self,log_file_path,output_file_path='output.json'):
         log_reg = re.compile(r'(.*?)\s\[(.*?)\]\s(.*?)\s(.*?)\s\[(.*?)\]\s\-\s(.*?)$')
-        learned_frames={}
-        with open(learning_log_file_path,'r',encoding='utf-8') as f:
+        result={}
+        result['frames']=[]
+        with open(log_file_path,'r',encoding='utf-8') as f:
             for line in f:
                 res = log_reg.findall(line)
                 log_type = res[0][4]
                 log_content = res[0][5]
                 if log_type == 'barcodeConfig':
-                    data['barcodeConfig'] = json.loads(log_content)
+                    result['barcodeConfig'] = json.loads(log_content)
                 elif log_type == 'fecParameters':
-                    data['fecParameters'] = json.loads(log_content)
+                    result['fecParameters'] = json.loads(log_content)
                 elif log_type == 'processed':
-                    current=json.loads(log_content)
-                    learned_frames[current['index']]=current
+                    result['frames'].append(json.loads(log_content))
                 elif log_type == 'sha1':
-                    data['file']['sha1'] = log_content
-        for frame in data['frames']:
-            index=frame['image']['index']
-            if index in learned_frames:
-                frame['rs']=learned_frames[index]['rs']
-                frame['raptorQ']=learned_frames[index]['raptorQ']
+                    result['file']={}
+                    result['file']['sha1'] = log_content
         with open(output_file_path, 'w', encoding='utf-8') as f:
-            f.write(json.dumps(data))
+            f.write(json.dumps(result))
 
 if __name__ == '__main__':
     format = Format()
-    #format.format_log('2017-06-22 20-15-02.txt', 'output.json')
-    format.format_log_learning('output.json','2017-06-26 18-05-42.txt','output.json')
+    #format.format_log('/Volumes/扩展存储/实验/BlackWhiteCodeWithBar/100x100_0.1/视频/28fps_50cm_4x/2017-06-28 13:39:09.txt')
+    format.format_log_learning('/Volumes/扩展存储/实验/BlackWhiteCodeML/60x60_0.1/视频/22fps_50cm_3x/1683/learning.txt')
